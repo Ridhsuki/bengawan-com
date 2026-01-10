9108351f87b057b719243cd85d99a24d660af850
Ridhsuki <basukiridhoalghifary@gmail.com>
2026-01-10 22:16:02 +0700

    feat(migration, model): add Product model and migration

---
+++ database/migrations/2026_01_10_150501_create_company_profiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('address');
+            $table->text('about');
+            $table->string('phone');
+            $table->string('email')->nullable();
+            $table->text('maps_link')->nullable();
+            $table->json('social_media')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_profiles');
+    }
+};
