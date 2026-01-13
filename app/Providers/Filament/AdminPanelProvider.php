abcde4b17f8feedd2df3c3c0e3d1fdf6af320d66
Ridhsuki <basukiridhoalghifary@gmail.com>
2026-01-13 23:03:38 +0700

    feat: implement collapsible sidebar

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->sidebarCollapsibleOnDesktop(true)
             ->id('admin')
             ->path('admin')
             ->login()
