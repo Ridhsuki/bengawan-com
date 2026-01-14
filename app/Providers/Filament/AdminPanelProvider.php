74d0ef2a9d41fc3dead8ede515cb92843be26ce8
Ridhsuki <basukiridhoalghifary@gmail.com>
2026-01-15 00:19:30 +0700

    fix: add favicon in admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('assets/favicon/favicon.ico'))
             ->profile(isSimple: false)
             ->colors([
                 'primary' => '#172D9D'
