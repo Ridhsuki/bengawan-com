7f9f16fd3ffad3dc0de2cdfc74b6da599eec4fd6
Ridhsuki <basukiridhoalghifary@gmail.com>
2026-01-22 23:29:31 +0700

    fix: notofount function and icon

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -84,6 +84,9 @@
                             seconds.
                         </div>
                     </div>
+                    <script>
+                        document.addEventListener("DOMContentLoaded", function() {var countdownElement = document.getElementById('retry-countdown');var seconds = parseInt(countdownElement.innerText);var timer = setInterval(function(){seconds--;if (seconds <= 0){clearInterval(timer);countdownElement.innerText = "0";countdownElement.parentElement.innerHTML = "You can send your message now.";} else {countdownElement.innerText = seconds;}}, 1000);});
+                    </script>
                 @endif
 
                 @if ($errors->has('message'))
